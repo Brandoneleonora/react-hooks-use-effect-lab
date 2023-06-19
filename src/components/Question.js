240bbf06c902020ef08fb48ddb8e498c76d28747
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-06-19 19:07:31 -0400

    complete project

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+
+   const countdownTimer = timeRemaining == 0 ? onAnswered(false) : setTimeout(() => {
+      setTimeRemaining(timeRemaining-1)
+    }, 1000)
+
+    return () => {
+      clearTimeout(countdownTimer)
+    }
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
